1678ad7a99556f894e4a3ce03b7aecbe6089490a
anthony <tapiaanthony81@gmail.com>
2021-09-25 03:30:16 -0400

    first commit

--- week-3-project/Untitled-1.js
+++ week-3-project/Untitled-1.js
@@ -1,8 +1,8 @@
 // Problem 1
 function oneToTen() {
-	// console.log('Ascending from 1 to 10');
+	console.log('Ascending from 1 to 10');
 	for (let i = 1; i <= 10; i++) {
-		// console.log(i);
+		console.log(i);
 	}
 }
 
@@ -18,8 +18,8 @@ while (number < 103) {
 	}
 	number += 1;
 }
-// console.log('The odd numbers between 3 and 103 not including 3 and 103 are');
-// console.log(oddArr);
+console.log('The odd numbers between 3 and 103 not including 3 and 103 are');
+console.log(oddArr);
 
 //Problem 3
 let sum = 0;
@@ -27,15 +27,15 @@ function arraySum(num) {
 	for (let i = 0; i < num.length; i++) {
 		sum = num[i] + sum;
 	}
-	// console.log(`Sum of the array is ${sum}`);
+	console.log(`Sum of the array is ${sum}`);
 }
 arraySum([ 1, 2, 3, 4, 5, 6 ]);
 
 //Problem 4
 let descendingNumber = 9;
-// console.log('Descending from 9 to 0');
+console.log('Descending from 9 to 0');
 while (descendingNumber > -1) {
-	// console.log(descendingNumber);
+	console.log(descendingNumber);
 	descendingNumber -= 1;
 }
 
@@ -45,31 +45,33 @@ function factorial(n) {
 	for (let i = n; i >= 1; i--) {
 		total = total * i;
 	}
-	// console.log(`The factorial of ${n} is ${total}`);
+	console.log(`The factorial of ${n} is ${total}`);
 }
 factorial(4);
-
+let arr = [];
 //Problem 6
-// Write a function using a for loop that returns the sum of all numbers from 1 to n.
-let arr6 = [];
-let problem6Sum = 0;
-
+sum = 0;
 function sumOfN(num) {
-	for (let i = num; i > 0; i--) {
-		arr6.push(1 * num);
+	let oriNum = num;
+	while (num > 0) {
+		arr.push(1 * num);
 		num -= 1;
 	}
-	console.log(arr6);
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+	}
+	console.log(`The sum of all numbers from 1 to ${oriNum} = ${sum}`);
 }
-sumOfN(5);
+// can we use multiple loops not just for loop?
+sumOfN(3);
 
 //problem 7
 let str = 'donuts';
-let arr = [];
+arr = [];
 arr = str.split('');
 for (let i = 1; i < str.length; i++) {
 	if (i % 2 != 0) {
 		arr[i] = 'Z';
 	}
 }
-// console.log(arr);
+console.log(arr);
